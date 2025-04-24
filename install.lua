be358b946b5a4b4af9b0c704c60bf3b066960ef9
jaamarcelis <jaa.marcelis@outlook.com>
2025-04-24 14:11:46 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/jaamarcelis/drmon/refs/heads/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/jaamarcelis/drmon/refs/heads/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
@@ -23,4 +23,3 @@ startupFile = startup.readAll()
 local file2 = fs.open("startup", "w")
 file2.write(startupFile)
 file2.close()
-
